dd6bec0f1a501b31affa41eb549d9a70e6e451a8
Pavel Druyan <pavel@druyan.net>
2018-08-14 14:41:31 +0300

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
 set -e
-
+BASE_URL=${BASE_URL:-/}
+if [ "${BASE_URL}" ]; then
+  sed -i "s|location .* {|location $BASE_URL {|g" /etc/nginx/nginx.conf
+fi
 INDEX_FILE=/usr/share/nginx/html/index.html
 
 # TODO: this is empty but we'll be adding configuration values here
