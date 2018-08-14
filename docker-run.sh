649ea9ad07855b1dbb77956f97dc896ea76dec4e
Pavel Druyan <paveld@il.ibm.com>
2018-08-14 15:02:43 +0300

    Add BASE_URL

--- docker-run.sh
+++ docker-run.sh
@@ -2,9 +2,11 @@
 
 set -e
 BASE_URL=${BASE_URL:-/}
+
 if [ "${BASE_URL}" ]; then
   sed -i "s|location .* {|location $BASE_URL {|g" /etc/nginx/nginx.conf
 fi
+
 INDEX_FILE=/usr/share/nginx/html/index.html
 
 # TODO: this is empty but we'll be adding configuration values here
